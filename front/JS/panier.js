caff52fd7ffdf6963d2e4e7adba4b4c2143e671e
Puru <pulageswaran@gmail.com>
2021-12-17 16:41:05 +0100

    début validation formulaire panier

--- front/JS/panier.js
+++ front/JS/panier.js
@@ -86,9 +86,9 @@ function getCart() {
                 </div>`
 
 
-                countProduct = countProduct + produit.quantiteProduit; 
-                totalPrice = totalPrice + (produit.prixProduit*produit.quantiteProduit);
-            
+            countProduct = countProduct + produit.quantiteProduit;
+            totalPrice = totalPrice + (produit.prixProduit * produit.quantiteProduit);
+
         }
 
         document.getElementById("totalQuantity").innerHTML = countProduct;
@@ -96,8 +96,184 @@ function getCart() {
 
 
     }
+    // Modification d'une quantité de produit
+    function modifyQuantity() {
+        let qttModif = document.getElementsByClassName("itemQuantity");
+
+        for (let k = 0; k < qttModif.length; k++) {
+            qttModif[k].addEventListener("change", (event) => {
+                event.preventDefault();
+
+                //Selection de l'element à modifier en fonction de son id ET sa couleur
+                let quantityModif = produitEnregistre[k].quantiteProduit;
+                let quantityChangeValue = qttModif[k].valueAsNumber;
+
+                const resultFind = produitEnregistre.find((element) => element.qttModifValue !== quantityModif);
+
+                resultFind.quantiteProduit = quantityChangeValue;
+                produitEnregistre[k].quantiteProduit = resultFind.quantiteProduit;
+
+                localStorage.setItem("produit", JSON.stringify(produitEnregistre));
+                location.reload();
+            })
+        }
+    }
+    modifyQuantity();
+    // Suppression d'un produit
+    function deleteProduct() {
+        let btn_supprimer = document.getElementsByClassName("deleteItem");
+
+        for (let i = 0; i < btn_supprimer.length; i++) {
+            btn_supprimer[i].addEventListener("click", (event) => {
+                event.preventDefault();
+
+                //Selection de l'element à supprimer en fonction de son id ET sa couleur
+                let idDelete = produitEnregistre[i].idProduit;
+                let colorDelete = produitEnregistre[i].couleurProduit;
+
+                produitEnregistre = produitEnregistre.filter(element => element.idProduit !== idDelete || element.couleurProduit !== colorDelete);
+
+                localStorage.setItem("produit", JSON.stringify(produitEnregistre));
+                location.reload();
+            })
+        }
+    }
+    deleteProduct();
+
+
+
 }
 getCart();
+
+/*
+//Instauration formulaire avec regex
+function getForm() {
+    // Ajout des Regex
+    let form = document.querySelector(".cart__order__form");
+
+    //Création des expressions régulières
+    let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
+    let charRegExp = new RegExp("^[a-zA-Z ,.'-]+$");
+    let addressRegExp = new RegExp("^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+");
+
+*/
+
+// Test ajout formulaire
+//function addForm() {
+const btn_commander = document.getElementById("order");
+btn_commander.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const clientInform = {
+
+        Prenom: document.getElementById("firstName").value,
+        Nom: document.getElementById("lastName").value,
+        Adresse: document.getElementById("address").value,
+        Ville: document.getElementById("city").value,
+        Email: document.getElementById("email").value
+    }
+
+    //validation prénom
+    function checkPrenom() {
+        const prenom = clientInform.Prenom;
+        if (/^[A-Za-z]{3,25}$/.test(prenom)) {
+            console.log("ok");
+            return true;
+        } else {
+            console.log("error");
+            alert("Veuillez contrôler les informations saisies")
+            return false
+        }
+    }
+
+    //validation nom
+    function checkNom() {
+        const nom = clientInform.Nom;
+        if (/^[A-Za-z]{3,25}$/.test(nom)) {
+            console.log("ok");
+            return true;
+        } else {
+            console.log("error");
+            alert("Veuillez contrôler les informations saisies")
+            return false
+        }
+    }
+
+
+    //validation ville
+    function checkVille() {
+        const ville = clientInform.Ville;
+        if (/^[A-Za-z]{3,25}$/.test(ville)) {
+            console.log("ok");
+            return true;
+        } else {
+            console.log("error");
+            alert("Veuillez contrôler les informations saisies")
+            return false
+        }
+    }
+
+    if (checkPrenom() && checkNom() && checkVille())  {
+        localStorage.setItem("clientInform", JSON.stringify(clientInform));
+    } else {}
+    //LS 
+
+
+    const envoiLs = {
+        produitEnregistre,
+        clientInform
+    }
+})
+
+/*
+        localStorage.setItem("prenom", document.getElementById("firstName").value);
+        localStorage.setItem("nom", document.getElementById("lastName").value);
+        localStorage.setItem("adresse", document.getElementById("address").value);
+        localStorage.setItem("ville", document.getElementById("city").value);
+        localStorage.setItem("email", document.getElementById("email").value);
+        
+        
+        const infoClient = {
+                
+                    Prenom: localStorage.getItem("prenom"),
+                    Nom: localStorage.getItem("nom"),
+                    Adresse: localStorage.getItem("adresse"),
+                    Ville: localStorage.getItem("ville"),
+                    Email: localStorage.getItem("email")
+                }
+
+                console.log(infoClient);
+
+    }
+    )
+
+    //envoyer vers LS
+    const aEnvoyer = {
+        produitEnregistre,
+        infoClient
+    }
+
+    */
+
+
+
+/*
+let idProducts = [];
+for (let i = 0; i<produitEnregistre.length;i++) {
+    idProducts.push(produitEnregistre[i].idProduit);
+}
+console.log(idProducts);
+*/
+
+//valeur à transférer dans l'array
+
+
+
+
+
+
+
+
 
 //calcul panier : 
 
@@ -115,13 +291,4 @@ document.getElementById("itemQuantity").value = quantiteProduit;
 window.onload = function () {
 document.getElementById("title").innerHTML = storageTitle;
 }
-*/
-
-
-/*
-window.onload = function() {
-    document.getElementById('videoBackground').onload = function() {
-        document.body.style.backgroundColor = 'black';
-    }
-};
 */
